2e495c10a75b29d3213f47a6d76f13e85a42a376
Aleyandroo <eacolaj@gmail.com>
2025-09-11 10:40:25 -0600

    implementacion main

--- Algoritmo.java
+++ Algoritmo.java
@@ -1,11 +1,13 @@
-public class Algoritmo
-{
-    float valor;
-    public Algoritmo(float x)
-    {
-        this.valor = x;
-    }
-    public float calcular(float input)
-    return input * valor;
+import java.util.List;
 
+/**
+ * Interfaz simple para algoritmos de control que puedan aplicarse
+ * a la lista de intersecciones del tráfico.
+ */
+public interface Algoritmo {
+    /**
+     * Aplica el algoritmo sobre la lista de intersecciones.
+     * Implementación mínima: puede ajustar duraciones de semáforos.
+     */
+    void aplicar(List<Interseccion> listaIntersecciones);
 }
--- Main.java
+++ Main.java
@@ -1,9 +1,19 @@
-public class Main
-{
-    public static void main(String []args)
-    {
-        Controlsim control = new Controlsim();
-        
+public class Main {
+    public static void main(String[] args) {
+        Trafico trafico = new Trafico(null); // sin algoritmo personalizado
+        Interseccion i1 = new Interseccion(1, "Central");
+        i1.addSensor(new Sensor(1));
+        i1.addSensor(new Sensor(2));
+        i1.addSemaforo(new Semaforo("ROJO", 10, 3, 13));
+        trafico.addInterseccion(i1);
+
+        Simulacion sim = new Simulacion();
+        ControlSim cs = new ControlSim(trafico, sim);
+        ControlUs cu = new ControlUs(cs);
+
+        cu.detectarEvento("INICIAR");
+        cu.detectarEvento("STEP");
+        Metrica m = trafico.obtenerMetricas();
+        m.mostrarResultados();
     }
 }
-    
