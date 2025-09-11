96569102b4a34b83c3f616cef12b938189b9ae62
JuanPablo251086 <cas251086@uvg.edu.gt>
2025-09-10 21:23:16 -0600

    Clases esqueleto
    Esqueleto de las clases metrica y trafico

---
+++ Metrica.java
@@ -0,0 +1,37 @@
+import java.util.List;
+
+public class Metrica {
+    private double tiempoPromedioEspera;
+    private int vehiculosProcesados;
+    private int embotellamientosReducidos;
+    private double consumoEnergia;
+
+    public Metrica() {
+    }
+
+    public void actualizarMetricas() {
+    }
+
+    public void mostrarResultados() {
+    }
+
+    public double getPromEspera() {
+        return 0.0;
+    }
+
+    public int getVehiculosProcesados() {
+        return 0;
+    }
+
+    public double getConsumoEnergia() {
+        return 0.0;
+    }
+
+    public int getEmbotellamientosReducidos() {
+        return 0;
+    }
+
+    public String toString() {
+        return "";
+    }
+}
---
+++ Trafico.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class Trafico {
+    private Algoritmo algoritmoControl;
+    private ArrayList<Interseccion> listaDeIntersecciones;
+
+    public Trafico(Algoritmo algoritmoControl) {
+    }
+
+    public void actualizarEstado() {
+    }
+
+    public void calcularDuraciones() {
+    }
+
+    public Metrica obtenerMetricas() {
+        return null;
+    }
+
+    public void addInterseccion(Interseccion interseccion) {
+    }
+
+    public String toString() {
+        return "";
+    }
+}
