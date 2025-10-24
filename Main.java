f8e019fb2c3619ee20cbf135e2b12dceefbf5e5f
JuanPablo251086 <cas251086@uvg.edu.gt>
2025-10-24 09:58:44 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,17 +1,15 @@
 public class Main
 {
-    public static int maxR = 5;
-    public static int maxC = 5;
-
+    public static int maxR = 5; //Configuracion global del tamaño del mapa, generaliza bien
+    public static int maxC = 5; //recomiendo usar col / row pares
     public static void main(String[] args)
-{
-    Controller controlador = new Controller(maxR, maxC);
-    controlador.inicializarintersecciones();
+    {
+        Controller controlador = new Controller(maxR, maxC);
+        controlador.inicializarintersecciones();
 
-    Interseccion inter = controlador.getInterseccion(3,3);
-    int[] vecinos = inter.CalcularVecinos();
-    controlador.printallvecinosynodos();
-
-    //si pueden alguien eche un chataso que les de condiciones inciiales aleatorias para empezar a probar el programa despues
-}
+        Interseccion inter = controlador.getInterseccion(3,3);
+        int[] vecinos = inter.CalcularVecinos();
+        controlador.printallvecinosynodos();
+        //si pueden alguien eche un chataso que les de condiciones inciiales aleatorias para empezar a probar el programa despues
+    }
 }
